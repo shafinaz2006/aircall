5d3fa05d09d679dbf9a6204581f24adee8f2f1fc
Shafinaz Shafique <shafinaz2006@gmail.com>
2021-12-04 08:52:34 -0800

    remove unused vars

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
-import { BrowserRouter as Router, Routes, Route, Navigate, HashRouter, Link} from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter, Link} from "react-router-dom";
 import Header from './Header.jsx';
 import Activities from './components/Activities';
 import IncomingCalls from './components/IncomingCalls';
@@ -25,7 +25,7 @@ const App = () => {
           <Link to='/incomingcalls' className='link' current={window.location.href.indexOf('incoming') !== -1} > Incomings </Link>
           <Link to='/archivedcalls' className='link' current={window.location.href.indexOf('archived') !== -1} >Archived </Link>
         </HeaderLinkWrapper>
-        
+
         <Routes>
           <Route path='/incomingcalls'  element={<IncomingCalls />} />
           <Route path='/calls/:id'  element={<CallDetail /> } />
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './css/body.css';
 import './css/app.css';
 import './css/header.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
+
 
 
 
